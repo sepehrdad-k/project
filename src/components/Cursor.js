923673f9dbe05efc00254dc119ec1e81342b4fb9
sepehrdad <sepehrdad_kashiri@yahoo.com>
2021-11-18 02:11:24 +0330

    project

--- src/components/Cursor.js
+++ src/components/Cursor.js
@@ -2,13 +2,17 @@ import React, { useRef, useEffect } from "react";
 
 const Cursor = () => {
   const CurserRef = useRef(null);
+  const CurserPosition = (e) => {
+    const X = e.clientX - CurserRef.current.clientWidth / 2;
+    const Y = e.clientY - CurserRef.current.clientHeight / 2;
+    CurserRef.current.style.top = `${Y}px`;
+    CurserRef.current.style.left = `${X}px`;
+  };
   useEffect(() => {
-    document.addEventListener("mousemove", (e) => {
-      const X = e.clientX - CurserRef.current.clientWidth / 2;
-      const Y = e.clientY - CurserRef.current.clientHeight / 2;
-      CurserRef.current.style.top = `${Y}px`;
-      CurserRef.current.style.left = `${X}px`;
-    });
+    document.addEventListener("mousemove", CurserPosition);
+    return () => {
+      document.removeEventListener("mousemove", CurserPosition);
+    };
   }, []);
 
   return (
